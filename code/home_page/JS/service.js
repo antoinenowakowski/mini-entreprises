cfc7e56a356ea00b03c01b427e53cf30dc7c0f24
antoinenowakowski <70375463+antoinenowakowski@users.noreply.github.com>
2021-10-29 14:32:48 +0200

    page banque d'image commencée & terminée plus le responsive, première page terminée, reste plus que le lien pour aller vers le google froms pour acheter le produit et responisve de la première page

--- code/home_page/JS/service.js
+++ code/home_page/JS/service.js
@@ -6,6 +6,7 @@ var btnService = document.getElementById("myBtn-service");
 // Get the <span> element that closes the modal
 var spanService = document.getElementsByClassName("close-service")[0];
 
+var body = document.body
 // When the user clicks the button, open the modal 
 btnService.onclick = function() {
   modalService.style.display = "block";
@@ -14,4 +15,10 @@ btnService.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 spanService.onclick = function() {
   modalService.style.display = "none";
+}
+
+body.onclick = function(event) {
+  if (event.target == modalService) {
+    modalService.style.display = "none";
+  }
 }
